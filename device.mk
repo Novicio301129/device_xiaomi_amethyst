105f0d96e34d4c88604825be279406bae9022733
Novicio301129 <novmalone@gmail.com>
2026-09-24 13:34:15 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -448,13 +448,6 @@ PRODUCT_PACKAGES += \
     firmware_wlan_mac.bin_symlink \
     firmware_WCNSS_qcom_cfg.ini_symlink
 
-# WFD / Screen Cast Packages
-PRODUCT_PACKAGES += \
-    libwfdnative \
-    libwfdmmsrc \
-    libwfdservice \
-    WfdCommon
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/vendor_cmd.xml:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/vendor_cmd.xml \
